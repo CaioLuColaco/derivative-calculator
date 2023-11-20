d4db86a4df4d99c7bee95c7d0ce07e44e6b02b0f
caio lucena <caio.lcolaco@gmail.com>
2023-11-20 19:22:39 -0300

    First commit

---
+++ index.py
@@ -0,0 +1,49 @@
+from pyfiglet import Figlet
+
+def gerar_ascii_art(texto):
+    f = Figlet(font='slant')  # Escolha uma fonte, 'slant' é apenas um exemplo
+    ascii_art = f.renderText(texto)
+    return ascii_art
+
+banner = gerar_ascii_art("Colaco Productions")
+print(banner)
+
+def calcular_derivada(expressao):
+    # Dividir a string pelos espaços em branco e remover espaços no início e no final
+    resposta = []
+    componentes = [elemento.strip() for elemento in expressao.split()]
+
+    for elemento in componentes:
+
+        if elemento == "-" or elemento == "+": # Separando sinais
+            resposta.append(elemento)
+
+        elif not "x" in elemento: # Derivando numeros sem variaveis
+            resposta.append("0")
+
+        else: # Derivando componentes com variáveis
+            numeros = elemento.split("x") # Separa a multiplicação do expoente
+            if numeros[1] == "" or numeros[1] == "^1": # Caso não tenha expoente ou seja igual a 1, retorna apenas o multiplicador
+                resposta.append(numeros[0].replace("*", ""))
+            else:
+                multiplicador = int(numeros[0].replace("*", ""))
+                expoente = int(numeros[1].replace("^", ""))
+
+                multiplicador = multiplicador * expoente
+                expoente = expoente - 1
+
+                resposta.append(f'{multiplicador}*x^{expoente}' )
+        
+    return resposta
+
+expressao = input("Insira a expressão para a qual deseja calcular a derivada (exemplo: 2*x^3 - 4*x^2 + 7*x + 1): ")
+print("Entrada:")
+print(expressao)
+
+print_result = lambda expressao: print(*calcular_derivada(expressao), sep=' ')
+
+print("Resposta:")
+print_result(expressao)
+
+# Trigonométricas
+# (x^2 + x^3 + x) * (x^2 + x)
